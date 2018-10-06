907b8d880097ac67b92d45a1ca6a1c093a1d1430
Leandro Marino <leandrooliveiramarino@gmail.com>
2018-10-05 22:23:34 -0300

    Fix user's list bug

--- src/App.js
+++ src/App.js
@@ -19,10 +19,48 @@ class App extends Component {
         <h1 className='title'>Ranking</h1>
         <div className='row'>
             <AddUser handleSubmit={this.handleSubmit} formMessage={this.state.formMessage}/>
-            <UsersList users={this.state.userList}/>
+            <UsersList users={this.state.userList} toggleGamesPlayed={this.toggleGamesPlayed}/>
         </div>
       </React.Fragment>
     );
+  }
+
+  handleNumberPlayedGameTo = (show) => {
+
+    const userList = Object.keys(this.state.userList).reduce((carry, username) => {
+      const user = this.state.userList[username];
+      user.formattedNumberPlayedGames = show
+        ? this.state.userList[username].numberPlayedGames
+        : '*';
+
+      if(!carry) {
+        return {
+          [username]: user
+        }
+      }
+
+      carry[username] = user;
+      return carry;
+    }, null);
+
+    this.setState((prevState) => {
+      prevState.userList = userList;
+      return prevState;
+    })
+  }
+
+  toggleGamesPlayed = (event) => {
+    const button = event.target;
+    const show = Number(button.dataset.show);
+    this.handleNumberPlayedGameTo(!show);
+
+    button.innerText = !show
+      ? 'Hide the Number of Games Played'
+      : 'Show the Number of Games Played'
+
+    button.dataset.show = show
+      ? 0
+      : 1;
   }
 
   handleSubmit = (event) => {
@@ -31,13 +69,23 @@ class App extends Component {
     const formData = {
       firstName: form.elements['firstName'].value,
       lastName: form.elements['lastName'].value,
-      username: form.elements['username'].value
+      username: form.elements['username'].value,
+      formattedNumberPlayedGames: 0,
+      numberPlayedGames: 0
     }
 
     if(!this.isFormValid(formData)) {
       this.handleFormMessage(
         'danger',
-        'Please, fill all the options above or username has already been taken'
+        'Please, fill all the options above.'
+      );
+      return;
+    }
+
+    if(this.state.userList.hasOwnProperty(formData.username)) {
+      this.handleFormMessage(
+        'danger',
+        'Username has already been taken.'
       );
       return;
     }
@@ -68,10 +116,6 @@ class App extends Component {
   }
 
   isFormValid = (formData) => {
-    if(this.state.userList.hasOwnProperty(formData.username)) {
-      return false;
-    }
-
     return !Object.keys(formData).filter((element) => {
       return formData[element] === '';
     }).length;
